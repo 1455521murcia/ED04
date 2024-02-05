273554070462b8825b6bcbfb896888c12d1f91ad
1455521murcia <159057776+1455521murcia@users.noreply.github.com>
2024-02-05 18:50:16 +0100

    Add files via upload

---
+++ GabrielGIT.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gabriel.git;
+
+/**
+ *
+ * @author andro
+ */
+public class GabrielGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int numeroUno = 1;
+        int numeroDos = 2;
+    }
+}
